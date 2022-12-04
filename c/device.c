2d129fa93a7eb79685e95a2e35041b28883079e6
John Allison <john123allison@gmail.com>
2022-12-04 15:13:37 -0500

    start c version

---
+++ c/device.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include "device.h"
+
+Device *new_device()
+{
+    Device *d = malloc(sizeof(Device));
+    d->program_counter = START_ADDRESS;
+
+    // Load font into memory
+    for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+    {
+        d->memory[FONTSET_START_ADDRESS + i] = fontset[i];
+    }
+
+    return d;
+}
+
+int rand_int()
+{
+    srand(time());
+    return rand();
+}
